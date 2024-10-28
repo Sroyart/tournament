7bf2a5f48a3a8330d2daa61a70548b24b8c5101a
Sroyart <94043858+Sroyart@users.noreply.github.com>
2024-10-28 15:57:37 +0100

    fix: add confirm password to safeParse()

--- src/lib/actions/auth/signUp.ts
+++ src/lib/actions/auth/signUp.ts
@@ -1,11 +1,29 @@
 "use server"
 
 import prisma from "@/lib/db"
+import { getUser } from "@/lib/script"
 import { saltAndHashPassword } from "@/utils/password"
 import { z } from "zod"
 
+const errorsArrayToObject = (errors: z.ZodIssue[]) =>
+  errors.reduce<Record<string, string[]>>((acc, error) => {
+    const [key]: (number | string)[] = error.path
+
+    if (!key) {
+      throw new Error("Key is missing")
+    }
+
+    acc[key] ??= []
+    acc[key].push(error.message)
+
+    return acc
+  }, {})
+
 export const register = async (
-  state: { email: string; password: string } | null,
+  prevState: {
+    email: string
+    password: string
+  } | null,
   formData: FormData,
 ) => {
   const registerSchema = z.object({
@@ -22,32 +40,16 @@ export const register = async (
   })
   const email = formData.get("email") as string
   const password = formData.get("password") as string
-  const result = registerSchema.safeParse({ email, password })
-  const errorsArrayToObject = (errors: z.ZodIssue[]) =>
-    errors.reduce<Record<string, string[]>>((acc, error) => {
-      const [key]: (number | string)[] = error.path
-
-      if (!key) {
-        throw new Error("Key is missing")
-      }
-
-      acc[key] ??= []
-      acc[key].push(error.message)
-
-      return acc
-    }, {})
+  const confirmPassword = formData.get("confirmPassword") as string
+  const result = registerSchema.safeParse({ email, password, confirmPassword })
 
   if (!result.success) {
     return { email, password, errors: errorsArrayToObject(result.error.errors) }
   }
 
-  const isEmailTaken = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  })
-
-  if (isEmailTaken) {
+  try {
+    await getUser(email)
+  } catch {
     return { email, password, errors: { email: ["Email is already taken"] } }
   }
 
