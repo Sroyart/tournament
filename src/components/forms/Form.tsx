77359ffe0f55bff8a18096aa7c9ffe2a2e356f08
Sroyart <94043858+Sroyart@users.noreply.github.com>
2024-10-29 18:07:53 +0100

    style: add margin to button

--- src/components/forms/Form.tsx
+++ src/components/forms/Form.tsx
@@ -11,7 +11,7 @@ const Form: React.FC<Props> = ({ formAction, children, isPending }) => (
     <form action={formAction}>
       <fieldset>{children}</fieldset>
       <button
-        className="bg-blue-500 hover:bg-blue-600 text-white w-full py-2 rounded-md disabled:bg-blue-950 disabled:text-blue-700"
+        className="bg-blue-500 hover:bg-blue-600 my-4 text-white w-full py-2 rounded-md disabled:bg-blue-950 disabled:text-blue-700"
         aria-disabled={isPending}
         disabled={isPending}
       >
